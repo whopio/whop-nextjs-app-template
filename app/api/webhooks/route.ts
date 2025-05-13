fb41103d58e1901048878daefbedf39924cf9ecc
Jordan Jantschulev <j.jantschulev@gmail.com>
2025-05-13 10:19:46 -0400

    add webhook example

---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,40 @@
+import { makeWebhookValidator } from "@whop/api";
+import type { NextFetchEvent, NextRequest } from "next/server";
+
+const validateWebhook = makeWebhookValidator({
+	webhookSecret: process.env.WHOP_WEBHOOK_SECRET ?? "fallback",
+});
+
+export async function POST(
+	request: NextRequest,
+	fetchEvent: NextFetchEvent,
+): Promise<Response> {
+	// Validate the webhook to ensure it's from Whop
+	const webhookData = await validateWebhook(request);
+
+	// Handle the webhook event
+	if (webhookData.action === "payment.succeeded") {
+		const { id, amount, currency, user } = webhookData.data;
+
+		console.log(
+			`Payment ${id} succeeded for ${user.email} (${user.id}) with amount ${amount} ${currency}`,
+		);
+
+		// if you need to do work that takes a long time, use waitUntil to run it in the background
+		fetchEvent.waitUntil(
+			potentiallyLongRunningHandler(user.id, amount, currency),
+		);
+	}
+
+	// Make sure to return a 2xx status code quickly. Otherwise the webhook will be retried.
+	return new Response("OK", { status: 200 });
+}
+
+export async function potentiallyLongRunningHandler(
+	_user_id: string,
+	_amount: number,
+	_currency: string,
+) {
+	// This is a placeholder for a potentially long running operation
+	// In a real scenario, you might need to fetch user data, update a database, etc.
+}
