913741f47af90aacfce09bcf80be2892140d3848
Jack Sharkey <sharkeyjack11@gmail.com>
2025-05-27 13:55:56 +0200

    Changes

---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+// TODO: run pnpm prisma:generate and pnpm prisma:db:push to generate the prisma client and push the schema to the database
+
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
